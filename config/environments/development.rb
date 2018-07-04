8344127c2560e6362ffef7432a4354efcebde312
Ando Emerencia <ando.emerencia@gmail.com>
2018-07-04 12:58:47 +0200

    ae changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  require 'sidekiq/testing'
-  Sidekiq::Testing.inline!
+  #require 'sidekiq/testing'
+  #Sidekiq::Testing.inline!
 end
