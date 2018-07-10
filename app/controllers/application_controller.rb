9f5b8a558047e058f5180ec46092de8381d7b3ef
Ando Emerencia <ando.emerencia@gmail.com>
2018-07-10 13:14:42 +0200

    have user set chrome extension url, fix failed to update content error, specify token and chrome extension id on user create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   after_action :allow_iframe
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-private
+  protected
+
+  def configure_permitted_parameters
+    # Permit the `subscribe_newsletter` parameter along with the other
+    # sign up parameters.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :token])
+  end
+
+  private
 
   def allow_iframe
     response.headers.except! 'X-Frame-Options'
